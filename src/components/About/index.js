5fb503fd717d8a0041a0f1f6132bb74e35e81287
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-10-11 21:02:13 -0500

    updated css stylings

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,9 +4,9 @@ import selfie from "../../assets/selfie.jpeg";
 function About() {
     return (
         <section className='my-5'>
-            <h1 id="about">About Me</h1>
             < img src={selfie} className="my-2" style={{ width: "25%" }} alt="cover" />
-            <p id="bio">My name is Guadalupe Rocha and I am based out of Houston, TX. I grew up in south Texas, by the border, and moved to Houston to attend college. I have a B.A. in English/Linguistics and am now enrolled in a coding boot camp at Rice University in order to become a full stack web developer.</p>
+            <h1 className="about">About Me</h1>
+            <p className="bio">My name is Guadalupe Rocha and I am based out of Houston, TX. I grew up in south Texas, by the border, and moved to Houston to attend college. I have a B.A. in English/Linguistics and am now enrolled in a coding boot camp at Rice University in order to become a full stack web developer.</p>
         </section>
     );
 }
