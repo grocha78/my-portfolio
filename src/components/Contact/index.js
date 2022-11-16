edab1c9a382f5c648a6ddfe75a7765fab3565068
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-11-16 09:49:12 -0600

    updates

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,64 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
 
-import { validateEmail } from "../../utils/helpers";
+// import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const [errorMessage, setErrorMessage] = useState('');
-    const { name, email, message } = formState;
+    // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    // const [errorMessage, setErrorMessage] = useState('');
+    // const { name, email, message } = formState;
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        if (!errorMessage) {
-            setFormState({ [e.target.name]: e.target.value });
-            console.log('Form', formState);
-        }
-    };
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     if (!errorMessage) {
+    //         setFormState({ [e.target.name]: e.target.value });
+    //         console.log('Form', formState);
+    //     }
+    // };
 
-    function handleChange(e) {
-        if(e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            console.log(isValid);
-            if (!isValid) {
-                setErrorMessage('Your email is invalid');
-            } else {
-                setErrorMessage('');
-            }
-        } else {
-            if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
-            } else {
-                setErrorMessage('');
-            }
-        }
-    };
+    // function handleChange(e) {
+    //     if(e.target.name === 'email') {
+    //         const isValid = validateEmail(e.target.value);
+    //         console.log(isValid);
+    //         if (!isValid) {
+    //             setErrorMessage('Your email is invalid');
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     } else {
+    //         if (!e.target.value.length) {
+    //             setErrorMessage(`${e.target.name} is required.`);
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     }
+    // };
 
     return (
         <section>
-            <h2>Contact Me</h2>
-            <form className="contact-form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <br></br>
-                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="email">Email address:</label>
-                    <br></br>
-                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <br></br>
-                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className="error-text">{errorMessage}</p>
-                    </div>
-                )}
-                <button type="submit">Submit</button>
-            </form>
+            <p>Contact me by <a href="mailto:rochalupe@gmail.com">EMAIL</a></p>
+            <p>or visit me on my socials below 👇 </p>
         </section>
     );
 }
