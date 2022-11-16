62a0eac574a5d25fb63b46004b4d7e991f9327b1
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-11-16 08:52:57 -0600

    about bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,10 +7,12 @@ function About() {
             < img src={selfie} className="my-2" style={{ width: "20%" }} alt="cover" />
             <h2 className="about">About Me</h2>
             <p className="bio">My name is Guadalupe Rocha and I am based out of Houston, TX. I grew up in south Texas, by the border, and moved to Houston to attend college. 
-            I have a B.A. in English/Linguistics from the University of Houston and just earned a certificate in Full Stack Web Development from the Rice University Coding Boot Camp.
-            As a front-end web developer, I am leveraging my background in linguistics to provide unique perspectives on how end-users interact with websites and software platforms. 
-            I am an innovative problem-solver who is passionate about developing apps with a focus on mobile-first design and development. My strengths are in creativity, teamwork, 
-            and building projects from ideation to execution.</p>
+            I have a B.A. in English/Linguistics from the University of Houston and recently earned a certificate in full stack development from the Rice University Coding Bootcamp, 
+            with newly developed skills in JavaScript, CSS, React.js, and responsive web design. I’m known as an innovative problem solver that is passionate about developing apps, 
+            with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience. I worked on a team of 3 to 
+            develop a single-page MERN app that helps individuals track their food intake and health options on a daily basis. I’m excited to leverage my skills as part of a fast-paced, 
+            quality-driven team to build better experiences on the web. 
+</p>
         </section>
     );
 }
